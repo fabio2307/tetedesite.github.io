ead5f01ea91dcc236d82e4d8fa847f32dfd14d12
Fábio <fabio.so2307@hotmail.com>
2026-02-26 17:45:10 -0300

    ajustando carrossel

--- assets/js/blog-slider.js
+++ assets/js/blog-slider.js
@@ -1,28 +1,28 @@
 /*--------------- Blog Slider ---------------*/
 blogSwiper = new Swiper(".blog-slider", {
-    spaceBetween: 20,
-    speed: 600, // movimento mais suave
-    autoplay: {
+      spaceBetween: 20,
+      speed: 600, // movimento mais suave
+      autoplay: {
         delay: 4000,
         disableOnInteraction: false,
-    },
-    pagination: {
+      },
+      pagination: {
         el: ".swiper-pagination2",
         clickable: true,
-    },
+      },
 
-    breakpoints: {
+      breakpoints: {
         0: {
-            slidesPerView: 1,
-            loop: false, // 🔥 desativa loop no mobile
+          slidesPerView: 1,
+          loop: false, // 🔥 desativa loop no mobile
         },
         768: {
-            slidesPerView: 2,
-            loop: true,
+          slidesPerView: 2,
+          loop: true,
         },
         1024: {
-            slidesPerView: 3,
-            loop: true,
+          slidesPerView: 3,
+          loop: true,
         },
-    },
-});
+      },
+    });
