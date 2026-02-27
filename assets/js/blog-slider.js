7ed23fd1e19453f0e5734d50170b691bf2a59d8d
Fábio <fabio.so2307@hotmail.com>
2026-02-26 21:18:06 -0300

    ajustando um no .blog-slider

--- assets/js/blog-slider.js
+++ assets/js/blog-slider.js
@@ -1,11 +1,11 @@
 /*--------------- Blog Slider ---------------*/
 blogSwiper = new Swiper(".blog-slider", {
   spaceBetween: 20,
-  loop: posts.length > 3, // 🔥 só ativa loop se tiver mais de 3 posts
-  autoplay: posts.length > 1 ? {
-    delay: 3000,
+  loop: true,
+  autoplay: {
+    delay: 2500,
     disableOnInteraction: false,
-  } : false,
+  },
   pagination: {
     el: ".swiper-pagination2",
     clickable: true,
